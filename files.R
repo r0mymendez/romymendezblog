0d8062b8748f04cd44ddbf9222cc3e7dbe315ebf
romina-mendez <romina.elena.mendez@gmail.com>
2021-02-21 21:49:53 -0300

    first commit project

---
+++ files.R
@@ -0,0 +1,8 @@
+library(distill)
+library(postcards)
+
+create_article('blog')
+create_post("Introducción a Visualizaciones con R")
+create_post("Introducción a Visualizaciones con Python")
+create_post("CheatSheet SQL")
+file.create(".nojekyll")
