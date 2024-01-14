0927813ff5f3984b74ad3f944ef7e35fc2967798
r0mymendez <romina.elena.mendez@gmail.com>
2024-01-14 07:46:29 +0100

    update file and add the buymeacoffe in the last article

--- files.R
+++ files.R
@@ -20,5 +20,6 @@ create_post("Code Quality - SonarQube",collection ="posts_en")
 create_post("SQL to Python Pandas: A SQL User's Quick Guide",collection ="posts_en")
 create_post("Decoding a Data Model - Using SchemaSpy in Snowflake",collection ="posts_en")
 create_post("Transform your Pandas Dataframes",collection ="posts_en")
+create_post("SQL-Assistant-Text-to-SQL-Application-in-Streamlit",collection ="posts_en")
 
 
